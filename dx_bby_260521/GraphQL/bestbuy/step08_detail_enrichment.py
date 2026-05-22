5bbaffc6766d30800de1654e9e704548d77694c1
gomguard <knholic@gmail.com>
2026-05-22 13:46:25 +0900

    Pause near BestBuy similar section during detail render

--- dx_bby_260521/GraphQL/bestbuy/step08_detail_enrichment.py
+++ dx_bby_260521/GraphQL/bestbuy/step08_detail_enrichment.py
@@ -678,7 +678,11 @@ def detail_params():
             [
                 {"wait": 2000},
                 {"scroll_y": 1800},
-                {"wait": 800},
+                {"wait": 1500},
+                {"scroll_y": -500},
+                {"wait": 1000},
+                {"scroll_y": 500},
+                {"wait": 2500},
                 {"scroll_y": 1800},
                 {"wait": 800},
                 {"scroll_y": 1800},
